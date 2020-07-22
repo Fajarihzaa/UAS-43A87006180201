54277c74efbf9a0edb114bb54c2db51fdeb10079
Fajarihzaa <63348976+Fajarihzaa@users.noreply.github.com>
2020-07-22 23:51:47 +0700

    Update CustomersRepository.java

--- UAS_43A87006180201-Fajar Ihza Aryatama Tanjung/UAS_43A87006180201/src/main/java/com/northwind/jpa/repository/CustomersRepository.java
+++ UAS_43A87006180201-Fajar Ihza Aryatama Tanjung/UAS_43A87006180201/src/main/java/com/northwind/jpa/repository/CustomersRepository.java
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Repository;
 
 /**
  *
- * @author Rizukiwahyudi
+ * @author Fajar
  */
 @Repository
 public interface CustomersRepository extends JpaRepository<Customers, String>{    
